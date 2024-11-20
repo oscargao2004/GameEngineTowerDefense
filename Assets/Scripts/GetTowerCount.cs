e2701f073525deed6ba85ba5aa7f14e3dd63c1d1
oscargao2004 <74120655+oscargao2004@users.noreply.github.com>
2024-11-20 17:23:31 -0500

    Update GetTowerCount.cs

--- Assets/Scripts/GetTowerCount.cs
+++ Assets/Scripts/GetTowerCount.cs
@@ -1,7 +1,16 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GetTowerCount : Singleton
 {
+    
     public static List<GameObject> towers = new List<GameObject>();
+
+    public int towerCount;
+
+    private void Update()
+    {
+        towerCount = towers.Count;
+    }
 }
