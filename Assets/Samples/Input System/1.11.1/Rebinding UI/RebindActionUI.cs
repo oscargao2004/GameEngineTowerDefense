e27f5d9ff79d9c7388993b361df26139513bdf70
davidc1234otu <chuong.david1234@gmail.com>
2024-11-25 19:56:56 -0500

    Savable Keybinds

--- Assets/Samples/Input System/1.11.1/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.11.1/Rebinding UI/RebindActionUI.cs
@@ -236,6 +236,8 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
         /// </summary>
         public void StartInteractiveRebind()
         {
+            m_Action.action.Disable();
+
             if (!ResolveActionAndBinding(out var action, out var bindingIndex))
                 return;
 
@@ -260,7 +262,8 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             {
                 m_RebindOperation?.Dispose();
                 m_RebindOperation = null;
-                action.Enable();
+                m_Action.action.Enable();
+                SaveActionBinding();
             }
 
             //Fixes the "InvalidOperationException: Cannot rebind action x while it is enabled" error
@@ -434,6 +437,26 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             {
                 var action = m_Action?.action;
                 m_ActionLabel.text = action != null ? action.name : string.Empty;
+            }
+        }
+
+        private void Start()
+        {
+            LoadActionBindings();
+        }
+
+        private void SaveActionBinding()
+        {
+            var currentBindings = actionReference.action.actionMap.SaveBindingOverridesAsJson();
+            PlayerPrefs.SetString(m_Action.action.name + bindingId, currentBindings);
+        }
+
+        private void LoadActionBindings()
+        {
+            var savedBindings = PlayerPrefs.GetString(m_Action.action.name + bindingId);
+            if (!string.IsNullOrEmpty(savedBindings))
+            {
+                actionReference.action.actionMap.LoadBindingOverridesFromJson(savedBindings);
             }
         }
 
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,22 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : Singleton
 {
-    Tower activeTower;
-    [SerializeField] GameObject towerPrefab;
-    [SerializeField] List<EditTowerButton> editTowerButtons;
-    TowerCommand _upgradeCommand, _sellCommand, _recallCommand; 
 
     void Start()
     {
-        activeTower = FindAnyObjectByType<Tower>();
-        _upgradeCommand = new UpgradeTowerCommand(activeTower);
-        _sellCommand = new SellTowerCommand(activeTower);
-        _recallCommand = new RecallTowerCommand(activeTower);
-
-       
-        editTowerButtons[0].GetComponent<Button>().onClick.AddListener(() => editTowerButtons[0].ExecuteTowerCommand(_upgradeCommand));
-        editTowerButtons[1].GetComponent<Button>().onClick.AddListener(() => editTowerButtons[1].ExecuteTowerCommand(_sellCommand));
-        editTowerButtons[2].GetComponent<Button>().onClick.AddListener(() => editTowerButtons[2].ExecuteTowerCommand(_recallCommand));
+        
     }
 
     void Update()
@@ -29,12 +17,6 @@ public class UIManager : Singleton
 
     }
 
-    public void SpawnTower()
-    {
-        GameObject newTower = Instantiate(towerPrefab);
-        activeTower = newTower.GetComponent<Tower>();
-    }
-    
 
     public void PlayGame()
     {
